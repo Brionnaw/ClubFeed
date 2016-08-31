dd323d3f0cd7e45b1de301cd6bcbc9427117a096
Brionna White <Brionnanwhite@gmail.com>
2016-08-31 15:35:30 -0700

    add newLocation modal

--- ngApp/js/controllers.ts
+++ ngApp/js/controllers.ts
@@ -1,10 +1,24 @@
 namespace app.Controllers {
-  export class HomeController {
-
-    constructor() {
-
+    export class HomeController {
+        public showModal(animalName: string) {
+            this.$uibModal.open({
+                templateUrl: '/templates/newLocation.html',
+                controller: 'DialogController',
+                controllerAs: 'modal',
+                resolve: {
+                    animalName: () => animalName
+                },
+                size: 'md'
+            });
+        }
+        constructor(private $uibModal: angular.ui.bootstrap.IModalService) { }
     }
-  }
-
-  angular.module('app').controller('HomeController', HomeController);
+    angular.module('app').controller('HomeController', HomeController);
+    class DialogController {
+        public ok() {
+            this.$uibModalInstance.close();
+        }
+        constructor(public animalName: string, private $uibModalInstance: angular.ui.bootstrap.IModalServiceInstance) { }
+    }
+    angular.module('app').controller('DialogController', DialogController);
 }
