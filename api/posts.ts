d64db0e36e5937b4bdb8e0cbdd39b2ae2e095422
Brionna White <Brionnanwhite@gmail.com>
2016-09-06 14:17:53 -0700

    add Post to database

--- api/posts.ts
+++ api/posts.ts
@@ -1,10 +1,34 @@
 // route file
  import express = require('express');
+ let mongoose = require('mongoose');
+
  let router = express.Router();
+
+  // models
+  let Post = mongoose.model('Post', { // "," seperate parameters, {pass in name of model , object w| properties, values types}
+    text: String,
+    dateCreated: Date,
+    // latitude: String,
+    // longitude: String
+    // add usernames
+  })
+
 
  router.post("/posts", function(req, res) { // express does not like fat arrow :-(;
    // use postman to check error with endpoints post request (localhost:3000/api/posts)
-   console.log(req.body)
+   let newPost = new Post({
+     text: req.body.text,
+     dateCreated:new Date()
+   })
+
+   newPost.save((err, res) => {
+     if(err){
+       console.log(err)
+     } else {
+       console.log(res)
+     }
+   })
+   
    res.send('success!');
  } )
 
